12fb23e5b22ebef783452592b7f99b460aaf5f9d
Spencer Lin <spen7148@gmail.com>
2024-10-14 18:31:27 -0400

    added early navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
 
   return (
     <>
+      <div className="navbar">
+    <a href="/" className="nav-item">Home</a>
+    <a href="/about" className="nav-item">About</a>
+    <a href="/contact" className="nav-item">Contact</a>
+  </div>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +34,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
 }
